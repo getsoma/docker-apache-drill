8dc798f9ba634c188df9d3a063354cc15e054d0a
Peter Quintas <peter@somaglobal.com>
2020-06-17 13:09:51 -0400

    Add api authenticator.

--- security/src/main/java/com/somaglobal/drill/security/APIUserAuthenticator.java
+++ security/src/main/java/com/somaglobal/drill/security/APIUserAuthenticator.java
@@ -1,50 +1,76 @@
-package com.somaglobal.drill.security;     
+package com.somaglobal.drill.security;
 
-   import org.apache.drill.common.config.DrillConfig;
-   import org.apache.drill.exec.exception.DrillbitStartupException;
-   import org.apache.drill.exec.rpc.user.security.UserAuthenticator;
-   import org.apache.drill.exec.rpc.user.security.UserAuthenticationException;
-   import org.apache.drill.exec.rpc.user.security.UserAuthenticatorTemplate;
+import org.apache.drill.common.config.DrillConfig;
+import org.apache.drill.exec.exception.DrillbitStartupException;
+import org.apache.drill.exec.rpc.user.security.UserAuthenticator;
+import org.apache.drill.exec.rpc.user.security.UserAuthenticationException;
+import org.apache.drill.exec.rpc.user.security.UserAuthenticatorTemplate;
 
-   import java.io.IOException;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 
-   /*
-   * Implement {@link org.apache.drill.exec.rpc.user.security.UserAuthenticator} for illustrating how to develop a custom authenticator and use it in Drill
-   */
-   @UserAuthenticatorTemplate(type = "myCustomAuthenticatorType")
-   public class APIUserAuthenticator implements UserAuthenticator {
+import java.io.IOException;
 
-   /**
+/*
+* Implement {@link org.apache.drill.exec.rpc.user.security.UserAuthenticator} for illustrating how to develop a custom authenticator and use it in Drill
+*/
+@UserAuthenticatorTemplate(type = "myCustomAuthenticatorType")
+public class APIUserAuthenticator implements UserAuthenticator {
+
+  /**
    * Setup for authenticating user credentials.
    */
-    @Override
-    public void setup(DrillConfig drillConfig) throws DrillbitStartupException {
-      // If the authenticator has any setup such as making sure authenticator provider servers are up and running or 
-      // needed libraries are available, it should be added here.
-    }
+  @Override
+  public void setup(DrillConfig drillConfig) throws DrillbitStartupException {
+    // If the authenticator has any setup such as making sure authenticator provider
+    // servers are up and running or
+    // needed libraries are available, it should be added here.
+  }
 
-   /**
+  /**
    * Authenticate the given <i>user</i> and <i>password</i> combination.
    *
    * @param userName
    * @param password
-   * @throws UserAuthenticationException if authentication fails for given user and password.
+   * @throws UserAuthenticationException if authentication fails for given user
+   *                                     and password.
    */
-    @Override
-    public void authenticate(String userName, String password) throws UserAuthenticationException {
-      if (!(password.equals(userName))) {
-    throw new UserAuthenticationException("For demostration purposes, please use same password as username");
-      }
-    }
+  @Override
+  public void authenticate(String userName, String password) throws UserAuthenticationException {
+    CredentialsProvider provider = new BasicCredentialsProvider();
+    UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(userName, password);
+    provider.setCredentials(AuthScope.ANY, credentials);
 
-   /**
-   * Close the authenticator. Used to release resources. Ex. LDAP authenticator opens connections to LDAP server,
-   * such connections resources are released in a safe manner as part of close.
+    HttpClient client = HttpClientBuilder.create().setDefaultCredentialsProvider(provider).build();
+
+    try {
+      HttpResponse response = client.execute(new HttpGet("http://app:5000/user/current"));
+      int statusCode = response.getStatusLine().getStatusCode();
+
+      if (HttpStatus.SC_OK != statusCode) {
+        throw new UserAuthenticationException(response.getStatusLine().getReasonPhrase());
+      }
+    } catch (IOException ex) {
+      throw new UserAuthenticationException(ex.getMessage());
+    }
+  }
+
+  /**
+   * Close the authenticator. Used to release resources. Ex. LDAP authenticator
+   * opens connections to LDAP server, such connections resources are released in
+   * a safe manner as part of close.
    *
    * @throws IOException
    */
-    @Override
-    public void close() throws IOException {
-      // Any clean up such as releasing files/network resources should be done here
-    }
-   }  
+  @Override
+  public void close() throws IOException {
+    // Any clean up such as releasing files/network resources should be done here
+  }
+}
